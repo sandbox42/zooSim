6545aaf7d66c4c3249b80187175473b493491130
Marcus Anderson <ourfilteraddress@hotmail.co.uk>
2017-11-07 15:54:52 +0000

    Added some unit tests

---
+++ ZooSim.AppTests/ElephantTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZooSim.App.Tests
+{
+    [TestClass()]
+    public class ElephantTests
+    {
+        [TestMethod()]
+        public void FeedMeTest()
+        {
+            // Arrange
+            var ellie = new Elephant();
+            ellie.Health = 50;
+            float expected = 55;
+            float actual;
+
+            // Act
+            ellie.FeedMe(10);
+            actual = ellie.Health;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void RetardTest()
+        {
+            // Arrange
+            var ellie = new Elephant();
+            ellie.Health = 50;
+            float expected = 45;
+            float actual;
+
+            // Act
+            ellie.Retard(10);
+            actual = ellie.Health;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ ZooSim.CommonTests/RandomNumberTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static ZooSim.Common.RandomNumber;
+
+namespace ZooSim.Common.Tests
+{
+    [TestClass()]
+    public class RandomNumberTests
+    {
+        [TestMethod()]
+        public void GetRandomTest()
+        {
+            // Arrange
+            int actual;
+
+            // Act
+            actual = GetRandom(HealthAction.Boost);
+
+            // Assert
+            Assert.IsTrue(actual >= 10 && actual <= 25);
+        }
+    }
+}
